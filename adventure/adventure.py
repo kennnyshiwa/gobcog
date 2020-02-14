fb2287e9c6f5e61e913a5154fc9081bdf808c06e
Drapersniper <27962761+drapersniper@users.noreply.github.com>
2020-02-14 20:03:28 +0000

    fix devreset for catch cooldown

--- adventure/adventure.py
+++ adventure/adventure.py
@@ -1355,6 +1355,8 @@ class Adventure(BaseCog):
                 return
             c.heroclass["ability"] = False
             c.heroclass["cooldown"] = 0
+            if "catch_cooldown" in c.heroclass:
+                c.heroclass["catch_cooldown"] = 0
             await self.config.user(ctx.author).set(c.to_json())
         await ctx.tick()
 
