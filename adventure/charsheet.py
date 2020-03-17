27e75a3f852c83104981aeb24b33683f1198e82c
Drapersniper <27962761+drapersniper@users.noreply.github.com>
2020-03-17 10:41:34 +0000

    fix set bonuses

--- adventure/charsheet.py
+++ adventure/charsheet.py
@@ -571,6 +571,9 @@ class Character(Item):
                     elif key in ["cpmult", "xpmult", "statmult"]:
                         if value > 1:
                             base[key] += value
+                        elif value >= 0:
+                            base[key] -= (1 - value)
+
         self.gear_set_bonus = base
 
     def __str__(self):
