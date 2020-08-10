050450426aed363ff771b333c1745f6cf8202155
Drapersniper <27962761+drapersniper@users.noreply.github.com>
2020-08-10 09:17:32 +0100

    bump

--- adventure/adventure.py
+++ adventure/adventure.py
@@ -231,7 +231,7 @@ class AdventureResults:
 class Adventure(commands.Cog):
     """Adventure, derived from the Goblins Adventure cog by locastan."""
 
-    __version__ = "3.3.5"
+    __version__ = "3.3.6"
 
     def __init__(self, bot: Red):
         self.bot = bot
